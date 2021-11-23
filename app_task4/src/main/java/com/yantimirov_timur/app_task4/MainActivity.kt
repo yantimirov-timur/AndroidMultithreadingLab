6dc5debccafdbc1256a3ba26fc17c281efd87b65
TimurYantimirov <tyantimirov@yandex.ru>
2021-11-23 13:25:22 +0300

    Added new app: Download image with fresco lib

---
+++ app_task4/src/main/java/com/yantimirov_timur/app_task4/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.yantimirov_timur.app_task4
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.facebook.drawee.backends.pipeline.Fresco
+import com.facebook.drawee.view.SimpleDraweeView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Fresco.initialize(this)
+        setContentView(R.layout.activity_main)
+
+        val uri: Uri =
+            Uri.parse("https://storge.pic2.me/cm/2560x1440/346/556612eab5b78.jpg")
+
+        val drawView = findViewById<SimpleDraweeView>(R.id.my_image_view)
+
+        val button = findViewById<Button>(R.id.btn)
+        button.setOnClickListener {
+
+            drawView.setImageURI(uri, this)
+        }
+
+    }
+}
